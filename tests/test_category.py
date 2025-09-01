f642f66cecc9ff4e1b40e97a0525d454134eb8d0
Ирина Судейкина <irinaasster@gmail.com>
2025-09-01 19:36:20 +0500

    add class Category + test

---
+++ tests/test_category.py
@@ -0,0 +1,17 @@
+def test_category_init(category1, category2) -> None:
+    """
+    Проверка инициализации класса Category
+    :param category1: фикстура экземпляра класса Category - 1
+    :param category2: фикстура экземпляра класса Category - 2
+    :return: ничего не возвращает
+    """
+    assert category1.name == "Смартфоны"
+    assert category1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert len(category1.products) == 3
+    
+    assert category2.name == "Телевизоры"
+    assert category2.description == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    assert len(category2.products) == 1
+    
+    assert category1.category_count == 2
+    assert category2.product_count == 4
